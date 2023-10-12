781105b5f0640be7ac19cb00c7a322614ef6a62e
EsterDahri <esterdahri@gmail.com>
2023-10-12 16:39:54 +0200

    add style

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 
 /*
 Descrizione
-Dobbiamo creare una web-app che permetta di leggere una lista di dischi presente nel nostro server.
+Dobbiamo creare una web-app che permetta di leggere una lista di dischi presente nel nostro server. ✔️
 
 Stack
 Html, CSS, VueJS (importato tramite CDN), axios, PHP ✔️
 
 Consigli
 Nello svolgere l’esercizio seguite un approccio graduale.
-Prima assicuratevi che la vostra pagina index.php (il vostro front-end) riesca a comunicare correttamente con il vostro script PHP (le vostre “API”).
-Solo a questo punto sarà utile passare alla lettura della lista da un file JSON.
+Prima assicuratevi che la vostra pagina index.php (il vostro front-end) riesca a comunicare correttamente con il vostro script PHP (le vostre “API”). ✔️
+Solo a questo punto sarà utile passare alla lettura della lista da un file JSON. ✔️
 
 Bonus (da fare entro domani prima della correzione)
 Al click su un disco, recuperare e mostrare i dati del disco selezionato. 
@@ -36,19 +36,21 @@ Al click su un disco, recuperare e mostrare i dati del disco selezionato.
     <div id="app">
 
         <header>
-
+            <div class="container">
+                <img width="50" class="py-2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/1024px-Spotify_logo_without_text.svg.png" alt="">
+            </div>
         </header>
 
         <main>
-            <div class="container">
-                <div class="row g-3 flex-wrap">
+            <div class="wrapper p-5">
+                <div class="row g-5 flex-wrap">
                     <div class="col-4" v-for="(disc, index) in discs" :key="index">
-                        <div class="card">
-                            <img width="200" :src="disc.poster" alt="poster of album" class="img-card-top">
-                            <div class="card-body">
-                                <h3 class="card-title">{{disc.title}}</h3>
-                                <small>{{disc.author}}</small>
-                                <p class="card-text">{{disc.year}}</p>
+                        <div class="card p-4 d-flex align-items-center justify-content-center">
+                            <img width="250" :src="disc.poster" alt="poster of album" class="img-card-top">
+                            <div class="text-white card-body p-0 d-flex flex-column align-items-center justify-content-center">
+                                <h5 class="p-2 text-center card-title ">{{disc.title}}</h5>
+                                <small class="">{{disc.author}}</small>
+                                <h6 class="card-text p-2 ">{{disc.year}}</h6>
                             </div>
                         </div>
                     </div>
